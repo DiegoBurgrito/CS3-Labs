3d0a90f8c42bcaf0836d852db8eda847dffbb3fd
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-01-23 07:53:37 -0600

    added method

--- Trees/src/BinarySearchTree.java
+++ Trees/src/BinarySearchTree.java
@@ -5,105 +5,98 @@
 //Lab  -
 
 import static java.lang.System.*;
+
 import java.util.LinkedList;
 
-public class BinarySearchTree
-{
-	private TreeNode root;
+public class BinarySearchTree {
+    private TreeNode root;
 
-	public BinarySearchTree()
-	{
-		root = null;
-	}
+    public BinarySearchTree() {
+        root = null;
+    }
 
-	public void add(Comparable val)
-	{
-		root = add(val, root);
-	}
+    public void add(Comparable val) {
+        root = add(val, root);
+    }
 
-	private TreeNode add(Comparable val, TreeNode tree)
-	{
-	   if(tree == null)
-			tree = new TreeNode(val);
+    private TreeNode add(Comparable val, TreeNode tree) {
+        if (tree == null)
+            tree = new TreeNode(val);
 
-		Comparable treeValue = tree.getValue();
-		int dirTest = val.compareTo(treeValue);
-		
-		if(dirTest < 0)
-			tree.setLeft(add(val, tree.getLeft()));
-		else if(dirTest > 0)
-			tree.setRight(add(val, tree.getRight()));
-		
-		return tree;
-	}
+        Comparable treeValue = tree.getValue();
+        int dirTest = val.compareTo(treeValue);
 
-   public void inOrder()
-	{
-		inOrder(root);
-		System.out.println("\n");
-	}
+        if (dirTest < 0)
+            tree.setLeft(add(val, tree.getLeft()));
+        else if (dirTest > 0)
+            tree.setRight(add(val, tree.getRight()));
 
-	private void inOrder(TreeNode tree)
-	{
-		if (tree != null){
-			inOrder(tree.getLeft());
-			System.out.print(tree.getValue() + " ");
-			inOrder(tree.getRight());
-		}
-	}
+        return tree;
+    }
 
-	// preOrder 
-	public String preOrder() {
-		return null;
-}
-	// postOrder 
-	
-	// revOrder
-	
-	// levelOrder - use a queue
-	
-	// zigzagOrder - hint below but could be solved in a different manner
-	// loop thru a stack and load all nodes to a new stack(loading is based on direction)
-	// set new stack to old and repeat
+    public void inOrder() {
+        inOrder(root);
+        System.out.println("\n");
+    }
 
-	// getNumLevels
+    private void inOrder(TreeNode tree) {
+        if (tree != null) {
+            inOrder(tree.getLeft());
+            System.out.print(tree.getValue() + " ");
+            inOrder(tree.getRight());
+        }
+    }
 
-	// getNumLeaves 
-	
-	// getWidth - insure this works right for the 2nd test case
-	
-	// getHeight 
-	
-	// getNumNodes 
-	
-	// isFull
-		
-	// contains
-	
-	// maxNode
-	
-	// minNode
-	
-	// getSmallest
-	
-	// getLargest
-	
-	// remove - follow adds set up very closely and check powerpoint if needed
-	// 1st case = no children
-	// 2nd case = one child
-	// 3rd case two children
-	
-	// ****BONUS**** 
-	// display like a tree
+    // preOrder
+    public String preOrder() {
+        return null;
+    }
+    // postOrder
+
+    // revOrder
+
+    // levelOrder - use a queue
+
+    // zigzagOrder - hint below but could be solved in a different manner
+    // loop thru a stack and load all nodes to a new stack(loading is based on direction)
+    // set new stack to old and repeat
+
+    // getNumLevels
+
+    // getNumLeaves
+
+    // getWidth - insure this works right for the 2nd test case
+
+    // getHeight
+
+    // getNumNodes
+
+    // isFull
+
+    // contains
+
+    // maxNode
+
+    // minNode
+
+    // getSmallest
+
+    // getLargest
+
+    // remove - follow adds set up very closely and check powerpoint if needed
+    // 1st case = no children
+    // 2nd case = one child
+    // 3rd case two children
+
+    // ****BONUS****
+    // display like a tree
 
 
-	public String toString()
-	{
-		return "";
-	}
+    public String toString() {
+        return "";
+    }
 
-	private String toString(TreeNode tree)
-	{
-		return "";
-	}
+    private String toString(TreeNode tree) {
+        return "";
+    }
 }
