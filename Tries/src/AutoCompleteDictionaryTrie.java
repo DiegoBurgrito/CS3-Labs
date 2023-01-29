ee5f10db1542f64c974bf3d15bb99a219be2e81a
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-01-28 21:26:58 -0600

    finished addWord, size, isWord. Need to finish predictCompletions

--- Tries/src/AutoCompleteDictionaryTrie.java
+++ Tries/src/AutoCompleteDictionaryTrie.java
@@ -1,129 +1,168 @@
-
-import java.util.List;
-import java.util.Set;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * An trie data structure that implements the Dictionary and the AutoComplete ADT
- * @author You
  *
+ * @author You
  */
-public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
+public class AutoCompleteDictionaryTrie implements Dictionary, AutoComplete {
 
     private TrieNode root;
     private int size;
 
 
-    public AutoCompleteDictionaryTrie()
-	{
-		root = new TrieNode();
-	}
+    public AutoCompleteDictionaryTrie() {
+        root = new TrieNode();
+        size = 0;
+    }
 
 
-	/** Insert a word into the trie.
-	 * You should ignore the word's case.
-	 * That is, you should convert the string to all lower case as you insert it. */
-	public boolean addWord(String word)
-	{
-	    //TODO: Implement this method.
+    /**
+     * Insert a word into the trie.
+     * You should ignore the word's case.
+     * That is, you should convert the string to all lower case as you insert it.
+     */
+    public boolean addWord(String s) {
+        //TODO: Implement this method.
+        if(s != null && !s.equals("") && !isWord(s)) {
+            String word = s.toLowerCase();
+            TrieNode node = root;
+            int i = 0;
+            while(i < word.length()){
+                if(!node.getValidNextCharacters().contains(word.charAt(i))){
+                    node = node.insert(word.charAt(i));
+                }
+                else if(node.getValidNextCharacters().contains(word.charAt(i))){
+                    node = node.getChild(word.charAt(i));
+                }
+                i++;
+            }
+            node.setEndsWord(true);
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Return the number of words in the dictionary.  This is NOT necessarily the same
+     * as the number of TrieNodes in the trie.
+     */
+    public int size() {
+        //TODO: Implement this method
+        return size;
+    }
 
 
+    /**
+     * Returns whether the string is a word in the trie
+     */
+    @Override
+    public boolean isWord(String s) {
+        // TODO: Implement this method
+        if (s != null && !s.equals("")) {
+            int i = 0;
+            String word = s.toLowerCase();
+            TrieNode node = root;
+            while (i < s.length()) {
+                if (node.getValidNextCharacters().contains(word.charAt(i))) {
+                    node = node.getChild(word.charAt(i));
+                } else {
+                    return false;
+                }
+                i++;
+            }
+            return node.endsWord();
+        }
+        return false;
+    }
 
-
-
-
-		return false;
-	}
-
-	/**
-	 * Return the number of words in the dictionary.  This is NOT necessarily the same
-	 * as the number of TrieNodes in the trie.
-	 */
-	public int size()
-	{
-	    //TODO: Implement this method
-	    return -1;
-	}
-
-
-	/** Returns whether the string is a word in the trie */
-	@Override
-	public boolean isWord(String s)
-	{
-	    // TODO: Implement this method
-
-
-
-
-
-
-            return false;
-	}
-
-	/**
-	 *  * Returns up to the n "best" predictions, including the word itself,
+    /**
+     * * Returns up to the n "best" predictions, including the word itself,
      * in terms of length
      * If this string is not in the trie, it returns null.
+     *
      * @param text The text to use at the word stem
-     * @param n The maximum number of predictions desired.
+     * @param n    The maximum number of predictions desired.
      * @return A list containing the up to n best predictions
-     */@Override
-     public List<String> predictCompletions(String prefix, int numCompletions)
-     {
-    	 // TODO: Implement this method
-    	 // This method should implement the following algorithm:
-    	 // 1. Find the stem in the trie.  If the stem does not appear in the trie, return an
-    	 //    empty list
-    	 // 2. Once the stem is found, perform a breadth first search to generate completions
-    	 //    using the following algorithm:
-    	 //    Create a queue (LinkedList) and add the node that completes the stem to the back
-    	 //       of the list.
-    	 //    Create a list of completions to return (initially empty)
-    	 //    While the queue is not empty and you don't have enough completions:
-    	 //       remove the first Node from the queue
-    	 //       If it is a word, add it to the completions list
-    	 //       Add all of its child nodes to the back of the queue
-    	 // Return the list of completions
+     */
+    @Override
+    public List<String> predictCompletions(String text, int n) {
+        // TODO: Implement this method
+        // This method should implement the following algorithm:
+        // 1. Find the stem in the trie.  If the stem does not appear in the trie, return an
+        //    empty list
+        // 2. Once the stem is found, perform a breadth first search to generate completions
+        //    using the following algorithm:
+        //    Create a queue (LinkedList) and add the node that completes the stem to the back
+        //       of the list.
+        //    Create a list of completions to return (initially empty)
+        //    While the queue is not empty and you don't have enough completions:
+        //       remove the first Node from the queue
+        //       If it is a word, add it to the completions list
+        //       Add all of its child nodes to the back of the queue
+        // Return the list of completions
 
+        if(findStem(text) != null){
+            TrieNode stem = findStem(text);
+            LinkedList<TrieNode> queue = new LinkedList<>();
+            List<String> completions = new ArrayList<>();
+            queue.add(stem);
+            while(!queue.isEmpty() && completions.size() < n){
+                TrieNode node = queue.poll();
+                if(node.endsWord()){
+                    completions.add(node.getText());
+                    for (Character c : node.getValidNextCharacters()) {
+                        queue.add(node.getChild(c));
+                    }
+                } else {
+                    for (Character c : node.getValidNextCharacters()) {
 
+                    }
+                }
+            }
+            return completions;
+        }
 
+        return new ArrayList<String>();
+    }
+    private TrieNode findStem(String text) {
+        if(text.equals("")){
+            return root;
+        }
+        int i = 0;
+        String word = text.toLowerCase();
+        TrieNode node = root;
+        while (i < text.length()) {
+            if (node.getValidNextCharacters().contains(word.charAt(i))) {
+                node = node.getChild(word.charAt(i));
+            } else {
+                return node;
+            }
+            i++;
+        }
+        return null;
+    }
+    // For debugging
+    public void printTree() {
+        printNode(root);
+    }
 
+    /**
+     * Do a pre-order traversal from this node down
+     */
+    public void printNode(TrieNode curr) {
+        if (curr == null)
+            return;
 
+        System.out.println(curr.getText());
 
-
-
-
-
-
-
-
-         return new ArrayList<String>();
-     }
-
- 	// For debugging
- 	public void printTree()
- 	{
- 		printNode(root);
- 	}
-
- 	/** Do a pre-order traversal from this node down */
- 	public void printNode(TrieNode curr)
- 	{
- 		if (curr == null)
- 			return;
-
- 		System.out.println(curr.getText());
-
- 		TrieNode next = null;
- 		for (Character c : curr.getValidNextCharacters()) {
- 			next = curr.getChild(c);
- 			printNode(next);
- 		}
- 	}
+        TrieNode next = null;
+        for (Character c : curr.getValidNextCharacters()) {
+            next = curr.getChild(c);
+            printNode(next);
+        }
+    }
 
 
 
