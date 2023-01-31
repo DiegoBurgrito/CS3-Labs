e7f9ec68695d773941725300e1d1b10afb092af2
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-01-31 10:21:13 -0600

    changes

--- kdTrees/src/PointSET.java
+++ kdTrees/src/PointSET.java
@@ -3,29 +3,27 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
+
+import java.util.*;
 
 public class PointSET {
 
     private Set<Point2D> set;
 
     public PointSET() {                     // construct an empty set of points 
-
+        set = new HashSet<>();
     }
 
-    public boolean isEmpty() {              // is the set empty? 
-
-        return false;
+    public boolean isEmpty() {              // is the set empty?
+        return set.isEmpty();
     }
 
     public int size() {                      // number of points in the set 
-        return 0;
+        return set.size();
     }
 
     public void insert(Point2D p) {             // add the point to the set (if it is not already in the set)
-
+        set.add(p);
     }
 
     public boolean contains(Point2D p) {           // does the set contain point p? 
@@ -35,12 +33,19 @@ public class PointSET {
     public void draw() {                    // draw all points to standard draw with p.draw()
         StdDraw.setPenRadius(0.01);
         StdDraw.setPenColor(StdDraw.BLACK);
-
+        for (Point2D point : set) {
+            StdDraw.point(point.x(), point.y());
+        }
     }
 
-    public Iterable<Point2D> range(RectHV rect) {           // all points that are inside the rectangle 
-
-        return null;
+    public Iterable<Point2D> range(RectHV rect) {           // all points that are inside the rectangle
+        Iterable<Point2D> iterable = (Iterable<Point2D>) set.iterator();
+        while(iterable.) {
+            if(!rect.contains(iterable.next())) {
+                iterable.remove();;
+            }
+        }
+        return iterable;
     }
 
     public Point2D nearest(Point2D p) {           // a nearest neighbor in the set to point p; null if the set is empty 
