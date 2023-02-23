0fe4b3b28261d7cd6d6b4ff5237f584d1ec9ca89
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-02-23 10:02:42 -0600

    finished ShortestPathMaze.java

--- Graphs/src/ShortestPathMaze.java
+++ Graphs/src/ShortestPathMaze.java
@@ -1,59 +1,50 @@
-import java.util.Scanner;
-
-import static java.lang.System.*;
+import java.util.Arrays;
 
 public class ShortestPathMaze {
     private int[][] maze;
     private int shortest;
+    private int[][] copy;
 
     public ShortestPathMaze() {
         maze = new int[10][10];
+        copy = new int[10][10];
         shortest = 0;
     }
 
     public ShortestPathMaze(int[][] m) {
         maze = m;
+        copy = new int[m.length][m[0].length];
+        updateCopy();
         shortest= 0;
     }
 
+    private void updateCopy() {
+        for (int x = 0; x < maze.length; x++) {
+            System.arraycopy(maze[x], 0, copy[x], 0, copy[0].length);
+        }
+    }
 
     public void checkForExitPath(int r, int c, int path) {
         if(isValid(r, c)) {
-            maze[r][c] = 2;
+            maze[r][c] = 9;
             if(c == maze[0].length - 1) {
                 maze[r][c] = 9;
                 if(shortest == 0 || path < shortest) {
                     shortest = path + 1;
+                    updateCopy();
                 }
-                return;
             }
             else {
                 checkForExitPath(r + 1, c, path + 1);
-                int down = shortest;
                 checkForExitPath(r, c + 1, path + 1);
-                int right = shortest;
                 checkForExitPath(r - 1, c, path + 1);
-                int up = shortest;
                 checkForExitPath(r, c - 1, path + 1);
-                int left = shortest;
+           
             }
             maze[r][c] = 1;
         }
     }
 
-    private int smallest(int down, int right, int up, int left) {
-        int smallest = down;
-        if(right < smallest) {
-            smallest = right;
-        }
-        if(up < smallest) {
-            smallest = up;
-        }
-        if(left < smallest) {
-            smallest = left;
-        }
-        return smallest;
-    }
     private boolean isValid(int x, int y) {
         return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 1;
     }
@@ -63,13 +54,14 @@ public class ShortestPathMaze {
 
     public String toString() {
         String output = "";
-        for (int[] row : maze) {
+        for (int[] row : copy) {
             for (int cell : row) {
                 output += cell + " ";
             }
             output += "\n";
 
         }
-        return shortest > 0 ? output + "exit found in " + shortest + " steps" : output + "exit not found";
+        return shortest > 0 ? output + "shortest path in " + shortest : output + "exit not found";
     }
+
 }
