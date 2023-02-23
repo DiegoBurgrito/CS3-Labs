5f8110dec6c699ec8e2976c628ce739206fa4464
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-02-22 23:01:28 -0600

    finished Graph.java & GraphRunner.java

--- Graphs/src/Graph.java
+++ Graphs/src/Graph.java
@@ -4,29 +4,49 @@
 //Class -  
 //Lab  -
 
-import java.util.Map;
 import java.util.TreeMap;
 
-public class Graph
-{
-	private TreeMap<String, String> map;
-	private boolean yahOrNay;
+public class Graph {
+    private TreeMap<String, String> map;
+    private boolean yahOrNay;
 
-	public Graph(String line)
-	{
-	}
+    public Graph(String line) {
+		map = new TreeMap<String, String>();
+		for (String s : line.split(" ")) {
+            if(!map.containsKey(s.charAt(0) + "")){
+                map.put(s.charAt(0) + "", s.substring(1));
+            } else {
+                map.put(s.charAt(0) + "", map.get(s.charAt(0) + "") + s.substring(1));
+            }
+            if(!map.containsKey(s.charAt(1) + "")){
+                map.put(s.charAt(1) + "", s.substring(0,1));
+            } else {
+                map.put(s.charAt(1) + "", map.get(s.charAt(1) + "") + s.charAt(0));
+            }
+        }
+        yahOrNay = false;
+    }
 
-	public boolean contains(String letter)
-	{
-	   return true;
-	}
+    public boolean contains(String letter) {
+        return map.containsKey(letter);
+    }
 
-	public void check(String first, String second, String placesUsed)
-	{
-	}
+    public void check(String first, String second, String placesUsed) {
+        if(!map.containsKey(first) || !map.containsKey(second)){
+            return;
+        }
+        if (map.get(first).contains(second)) {
+            yahOrNay = true;
+        } else {
+            for (int i = 0; i < map.get(first).length(); i++) {
+                if (!placesUsed.contains(map.get(first).charAt(i) + "")) {
+                    check(map.get(first).charAt(i) + "", second, placesUsed + map.get(first).charAt(i));
+                }
+            }
+        }
+    }
 
-	public String toString()
-	{
-		return "";
-	}
+    public String toString() {
+        return yahOrNay ? "yes" : "no";
+    }
 }
--- Graphs/src/GraphRunner.java
+++ Graphs/src/GraphRunner.java
@@ -7,18 +7,18 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import static java.lang.System.*;
 
-public class GraphRunner
-{
-	public static void main( String[] args ) throws IOException
-	{
-		Scanner file = new Scanner(new File("graph1.dat"));
-		int howManyTimes = file.nextInt();
-		file.nextLine();
-		for(int x=0; x<howManyTimes; x++)
-		{
-			//add code here
-		}
-	}
+public class GraphRunner {
+    public static void main(String[] args) throws IOException {
+        Scanner file = new Scanner(new File("graph1.dat"));
+        int howManyTimes = file.nextInt();
+        file.nextLine();
+        for (int x = 0; x < howManyTimes; x++) {
+            //add code here
+			Graph g = new Graph(file.nextLine());
+			String s = file.nextLine();
+			g.check(s.charAt(0) + "", s.charAt(1) + "", "");
+			System.out.println(s.charAt(0) + " connect to " + s.charAt(1) + " == " + g + "");
+        }
+    }
 }
