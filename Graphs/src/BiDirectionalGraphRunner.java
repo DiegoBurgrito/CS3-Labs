b18e2c0e536a353ef54fc9cf6c1a593f9df80e70
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-02-22 23:31:02 -0600

    finished BiDirectionalGraph.java & BiDirectionalGraphRunner.java

--- Graphs/src/BiDirectionalGraphRunner.java
+++ Graphs/src/BiDirectionalGraphRunner.java
@@ -8,11 +8,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeSet;
-import static java.lang.System.*;
 
-public class BiDirectionalGraphRunner
-{
-	public static void main( String[] args ) throws IOException
-	{
-	}
+public class BiDirectionalGraphRunner {
+    public static void main(String[] args) throws IOException {
+		Scanner file = new Scanner(new File("bidgraph.dat"));
+		int howManyTimes = file.nextInt();
+		file.nextLine();
+		for (int x = 0; x < howManyTimes; x++) {
+			BiDirectionalGraph g = new BiDirectionalGraph(file.nextLine());
+			String[] s = file.nextLine().split(" ");
+			g.check(s[0], s[1], new TreeSet<>());
+			System.out.println(s[0] + " CONNECTS TO " + s[1] + " == " + g);
+		}
+    }
 }
