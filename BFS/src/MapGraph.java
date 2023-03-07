733e5bebebb1eb8c78fa5ce97f15b91a6e004171
DiegoBurgrito <diegoburgueno0@gmail.com>
2023-03-07 06:27:08 -0600

    changes

--- BFS/src/MapGraph.java
+++ BFS/src/MapGraph.java
@@ -6,13 +6,10 @@ import java.util.*;
  */
 public class MapGraph {
 
-    private int numVertices, numEdges;
-    private Map<GeographicPoint, MapNode> vertexMap;
-    private Map<MapNode, MapNode> parentMap;
+    public int numVertices, numEdges;
+    public Map<GeographicPoint, MapNode> vertexMap;
+    public Map<MapNode, MapNode> parentMap;
 
-    /**
-     * Create a new empty MapGraph
-     */
     public MapGraph() {
         parentMap = new HashMap<>();
         vertexMap = new HashMap<>();
@@ -30,48 +27,25 @@ public class MapGraph {
         System.out.println("Num edges: " + theMap.getNumEdges());
         System.out.println(theMap.bfs(new GeographicPoint(1.0, 1.0), new GeographicPoint(8, -1)));
 
-        // uncomment for part 2
         System.out.println(theMap.dijkstra(new GeographicPoint(1.0, 1.0), new GeographicPoint(8, -1)));
         System.out.println(theMap.aStarSearch(new GeographicPoint(1.0, 1.0), new GeographicPoint(8, -1)));
 
     }
 
-    /**
-     * Get the number of vertices (road intersections) in the graph
-     *
-     * @return The number of vertices in the graph.
-     */
     public int getNumVertices() {
         return numVertices;
     }
 
-    /**
-     * Return the intersections, which are the vertices in this graph.
-     *
-     * @return The vertices in this graph as GeographicPoints
-     */
     public Set<GeographicPoint> getVertices() {
         return vertexMap.keySet();
     }
 
-    /**
-     * Get the number of road segments in the graph
-     *
-     * @return The number of edges in the graph.
-     */
     public int getNumEdges() {
+
         return numEdges;
     }
 
-    /**
-     * Add a node corresponding to an intersection at a Geographic Point
-     * If the location is already in the graph or null, this method does
-     * not change the graph.
-     *
-     * @param location The location of the intersection
-     * @return true if a node was added, false if it was not (the node
-     * was already in the graph, or the parameter is null).
-     */
+
     public boolean addVertex(GeographicPoint location) {
         if (location == null || parentMap.containsKey(location)) {
             return false;
@@ -83,41 +57,32 @@ public class MapGraph {
         return true;
     }
 
-    /**
-     * Adds a directed edge to the graph from pt1 to pt2.
-     * Precondition: Both GeographicPoints have already been added to the graph
-     *
-     * @param from     The starting point of the edge
-     * @param to       The ending point of the edge
-     * @param roadName The name of the road
-     * @param roadType The type of the road
-     * @param length   The length of the road, in km
-     * @throws IllegalArgumentException If the points have not already been
-     *                                  added as nodes to the graph, if any of the arguments is null,
-     *                                  or if the length is less than 0.
-     */
-    public void addEdge(GeographicPoint from, GeographicPoint to, String roadName, String roadType, double length) throws IllegalArgumentException {
+    public void addEdge(GeographicPoint from, GeographicPoint to, String roadName,
+                        String roadType, double length) throws IllegalArgumentException {
+
+
         MapNode n1 = vertexMap.get(from);
         MapNode n2 = vertexMap.get(to);
+
         if (n1 == null)
             throw new NullPointerException("addEdge: from:" + from + "is not in graph");
         if (n2 == null)
             throw new NullPointerException("addEdge: to:" + to + "is not in graph");
+
         addEdge(n1, n2, roadName, roadType, length);
     }
 
-    private void addEdge(MapNode n1, MapNode n2, String roadName, String roadType, double length) {
+    private void addEdge(MapNode n1, MapNode n2, String roadName,
+                         String roadType, double length) {
         MapEdge edge = new MapEdge(n1.getGP(), n2.getGP(), roadName, roadType, length);
         n1.addEdge(edge);
         numEdges++;
     }
 
     public List<GeographicPoint> bfs(GeographicPoint start, GeographicPoint goal) {
-
-        Set<MapNode> visited = new HashSet<>();
-        Queue<MapNode> queue = new LinkedList<>();
-        Map<MapNode, MapNode> parent = new HashMap<>();
-
+        Set<MapNode> visited = new HashSet<MapNode>();
+        Queue<MapNode> queue = new LinkedList<MapNode>();
+        Map<MapNode, MapNode> parent = new HashMap<MapNode, MapNode>();
 
         boolean pathFound = false;
 
@@ -129,7 +94,6 @@ public class MapGraph {
 
         while (!queue.isEmpty()) {
             MapNode currentNode = queue.poll();
-
 
             if (currentNode.toString().equals(goalNode.toString())) {
                 pathFound = true;
@@ -146,9 +110,7 @@ public class MapGraph {
             }
 
         }
-
         return getPath(startNode, goalNode, parent, pathFound);
-
     }
 
     private List<MapNode> getNeighborNodes(MapNode mn) {
@@ -183,8 +145,9 @@ public class MapGraph {
     public List<GeographicPoint> dijkstra(GeographicPoint start,
                                           GeographicPoint goal) {
 
-        Set<MapNode> visited = new HashSet<MapNode>();
+        HashSet<MapNode> visited = new HashSet<MapNode>();
         PriorityQueue<MapNode> priorityQueue = new PriorityQueue<>();
+        parentMap = null;
         parentMap = new HashMap<MapNode, MapNode>();
         boolean pathFound = false;
         MapNode startNode = vertexMap.get(start);
@@ -199,27 +162,36 @@ public class MapGraph {
 
         int count = 0;
         while (!priorityQueue.isEmpty()) {
-            MapNode currNode = priorityQueue.poll();
+
+            MapNode currentNode = priorityQueue.poll();
             count++;
-            if (currNode.toString().equals(goalNode.toString())) {
+
+            if (currentNode.toString().equals(goalNode.toString())) {
                 pathFound = true;
                 break;
+                //continue;
             }
+            if (!visited.contains(currentNode)) {
+                visited.add(currentNode);
+                for (MapEdge edge : currentNode.getEdges()) {
+                    MapNode neighbor = vertexMap.get(edge.end);
+                    if (!visited.contains(neighbor)) {
+                        Double minDist = currentNode.getDistance() + edge.distance;
+                        if (minDist < neighbor.getDistance()) {
+                            neighbor.setDistance(minDist);
+                            parentMap.put(neighbor, currentNode);
+                            priorityQueue.add(neighbor);
+                        }
 
-            for (MapNode neighbor : getNeighborNodes(currNode)) {
-                if (!visited.contains(neighbor)) {
-                    double distance = currNode.getDistance() + currNode.getDistance();
-                    if (distance < neighbor.getDistance()) {
-                        neighbor.setDistance(distance);
-                        parentMap.put(neighbor, currNode);
-                        priorityQueue.add(neighbor);
                     }
                 }
             }
-            visited.add(currNode);
+
         }
         System.out.println("Dijkstra count: " + count);
+        //printDistMap(distanceMap);
         List<GeographicPoint> path = getPath(startNode, goalNode, parentMap, pathFound);
+        //printPath(path);
         return path;
     }
 
@@ -262,31 +234,39 @@ public class MapGraph {
 
         priorityQueue.add(startNode);
 
-        int cnt = 0;
+        int count = 0;
 
         while (!priorityQueue.isEmpty()) {
             MapNode currentNode = priorityQueue.poll();
-            cnt++;
+            count++;
 
-            if (currentNode.toString().equals(goalNode.toString())) {
-                pathFound = true;
-                break;
-            }
+            if (!visited.contains(currentNode)) {
+                visited.add(currentNode);
 
-            for (MapNode neighbor : getNeighborNodes(currentNode)) {
-                if (!visited.contains(neighbor)) {
-                    double distance = currentNode.getDistance() + currentNode.getDistance();
-                    if (distance < neighbor.getDistance()) {
-                        neighbor.setDistance(distance);
-                        neighbor.setHeuristicDist(distance + neighbor.getGP().distance(goal));
-                        parentMap.put(neighbor, currentNode);
-                        priorityQueue.add(neighbor);
+                if (currentNode.equals(goalNode)) {
+                    pathFound = true;
+                    break;
+                }
+
+                for (MapEdge edge : currentNode.getEdges()) {
+                    MapNode neighbor = vertexMap.get(edge.end);
+                    if (!visited.contains(neighbor)) {
+                        double heurDist = currentNode.getHeuristicDist() + edge.distance;
+                        double minDist = heurDist + (neighbor.getGP()).distance(goalNode.getGP());
+                        if (minDist < neighbor.getDistance()) {
+                            neighbor.setHeuristicDist(heurDist);
+                            neighbor.setDistance(minDist);
+                            parentMap.put(neighbor, currentNode);
+                            priorityQueue.add(neighbor);
+                        }
                     }
                 }
             }
-            visited.add(currentNode);
+
         }
-        System.out.println("Astar count: " + cnt);
+
+        System.out.println("Astar count: " + count);
+
         return getPath(startNode, goalNode, parentMap, pathFound);
     }
 }
@@ -347,7 +327,6 @@ class MapNode implements Comparable<MapNode> {
         heuristicDist = cost;
     }
 
-    @Override
     public String toString() {
         return gp.toString();
     }
